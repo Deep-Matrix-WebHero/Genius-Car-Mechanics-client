3e6774e0b58e8e9a2a29cca21354a3fc30782949
Deep-Matrix-WebHero <ataurrahmannur@gmail.com>
2022-01-28 17:49:07 +0600

    first commit

---
+++ src/Pages/Booking/Booking/Booking.js
@@ -0,0 +1,21 @@
+import React, {useState} from "react";
+import {useEffect} from "react";
+import {useParams} from "react-router";
+
+const Booking = () => {
+  const {serviceId} = useParams();
+  const [service, setService] = useState({});
+  useEffect(() => {
+    fetch(`http://localhost:5000/services/${serviceId}`)
+      .then((res) => res.json())
+      .then((data) => setService(data));
+  }, []);
+  return (
+    <div>
+      <h2>Detailsof :{service.name}</h2>
+      <h2>this is booking: {serviceId}</h2>
+    </div>
+  );
+};
+
+export default Booking;
